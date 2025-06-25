f40a876c7a59703891cbadb901675a174d456636
Hyakky-Maru <skblezze@gmail.com>
2025-06-25 13:40:06 -0500

    cambio enemigo

--- src/Enemigo.java
+++ src/Enemigo.java
@@ -2,8 +2,8 @@
 public class Enemigo {
 	//Atributos
 	public String tipo;
-	private int salud;
-	private int ataque;
+	private int salud=1;
+	private int ataque=1;
 	//Constructor
 	public Enemigo(String tipo, int salud, int ataque) {
 		this.tipo=tipo;
@@ -11,6 +11,26 @@ public class Enemigo {
 		this.ataque=ataque;
 	}
 	//Metodos
+	public void Atacar(int ataque) {
+		
+	}
+	public void RecibirDaño(int daño) {
+		
+	}
+	public boolean EstaVivo(int enemigo) {
+		boolean sivive=true;
+		if(salud!=1) {
+			return sivive=false;
+		} else {}
+		return sivive;
+	}
+	public int MostrarEstado(int salud) {
+		if(getSalud()>0) {
+			String cantsalud=String.format("Salud: %d"+ getSalud());
+		}
+		return 1;
+		
+	}
 	public String getTipo() {
 		return tipo;
 	}
@@ -18,7 +38,7 @@ public class Enemigo {
 		this.tipo=tipo;
 	}
 	public int getSalud() {
-		return salud;
+		return this.salud;
 	}
 	public void setSalud(int salud) {
 		this.salud = salud;
--- src/Prueba_enemigo.java
+++ src/Prueba_enemigo.java
@@ -1,11 +1,12 @@
 
 public class Prueba_enemigo {
 
-	public static void main(String[] args) {
+	public int main(String[] args) {
 		Enemigo enemigo1=new Enemigo("Atacante",1,1);
 		System.out.println("Tipo: " + enemigo1.getTipo());
 		System.out.println("Salud: " + enemigo1.getSalud());
 		System.out.println("Ataque: " + enemigo1.getAtaque());
+		return 0;
 		
 	}
 
